6ec6e9ad8a37fb9f15acd3d2b50b3aefd553b62e
Saravanan Kathiresan <saravanan.kathiresan@viacomcontractor.com>
2016-09-10 16:30:31 -0400

    Modified CORS header

--- app/app.js
+++ app/app.js
@@ -8,7 +8,7 @@ var express = require('express'),
 
 var allowCrossDomain = function(req, res, next) {
   
-  res.header('Access-Control-Allow-Origin', '*.tekaptitude.com');
+  res.header('Access-Control-Allow-Origin', 'http://www.tekaptitude.com');
   res.header('Access-Control-Allow-Methods', 'GET,PUT,POST,DELETE');
   res.header('Access-Control-Allow-Headers', 'Content-Type');
 
